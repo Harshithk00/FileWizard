cccf8be583d8248f64f7fe3f89132e078d081be4
Harshit <61313363+Harshithk00@users.noreply.github.com>
2025-03-30 03:43:24 +0530

    file-table

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,13 @@
 import { Pool } from "pg";
 
 export const poolAuth = new Pool({ connectionString: process.env.DATABASE_URL });
+
+
+/*CREATE TABLE files (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    file_name TEXT NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+*/
