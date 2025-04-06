2893a6aecc6f464a6dfacdc9d18d82ff30ebbefc
Harshit <61313363+Harshithk00@users.noreply.github.com>
2025-04-07 01:53:18 +0530

    2

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,11 @@
 import { Pool } from "pg";
 
-export const poolAuth = new Pool({ connectionString: process.env.DATABASE_URL });
+export const poolAuth = new Pool({ connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // 
+      },
+ }
+);
 
 
 /*CREATE TABLE files (
